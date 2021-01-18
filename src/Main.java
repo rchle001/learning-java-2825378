f514141ed7a747218a76f3cd3b0bd50b5d5c2b5a
Raymond <rchle001@ucr.edu>
2021-01-18 10:54:46 -0800

    Reference data types

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+        // Primitive Data Types
         int studentAge = 15;
         double studentGPA = 3.45;
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
 
+        // Reference Data Types
+        String StudentName = "Kayla Hammon";
+
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(StudentName);
     }
 }
