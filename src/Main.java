c5379b1b72acb10c64f2fd3386026d2e62822acf
Raymond <rchle001@ucr.edu>
2021-01-18 10:46:59 -0800

    Finished the stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int StudentAge = 15;
+        double StudentGPA = 3.45;
+        char StudentFirstInitial = 'K';
+        char StudentLastInitial = 'H';
+        boolean HasPerfectAttendance = true;
+
+        System.out.println(StudentAge);
+        System.out.println(StudentGPA);
+        System.out.println(StudentFirstInitial);
+        System.out.println(StudentLastInitial);
+        System.out.println(HasPerfectAttendance);
     }
 }
