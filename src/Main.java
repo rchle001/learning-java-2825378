11813a17352225ec6aa6a29932db44e1cf8f3a1d
Raymond <rchle001@ucr.edu>
2021-01-25 10:33:20 -0800

    Coding CHallenge completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
+    public static double EmployeeGrossYearlySalary(double WeeklyHours, double MoneyPerHour)
+    // input 1: number of hours the employee works per week 
+    // input 2: amount of money the employee make per hour
+    {
+        // roughly 52 weeks in a year
+        double YearlySalary = WeeklyHours * 52 * MoneyPerHour;  
 
+        return YearlySalary;
+    }
     public static void main(String[] args) {
-        
+        double salary = EmployeeGrossYearlySalary(40, 20);
+        System.out.println(salary);
     }
 
 }
