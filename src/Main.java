6411eab5e32bce92dfb4e70e7e02275f95a8b714
Raymond <rchle001@ucr.edu>
2021-01-25 10:18:46 -0800

    implemented function with parameters/arguments; called function in main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double ListedMealPrice, double TipRate, double TaxRate) {
+        double tip = TipRate * ListedMealPrice;
+        double tax = TaxRate * ListedMealPrice;
+        double result = ListedMealPrice + tip + tax;
+        System.out.println("Your total meal price is " + result);
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(15, .2, .08);
+        calculateTotalMealPrice(25, .18, .08);
     }
 
 }
