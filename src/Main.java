f28014846b8a97e1530bfd7a243816662efb625a
Raymond <rchle001@ucr.edu>
2021-01-25 15:15:59 -0800

    Instanced 2 Triangle objects

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Triangle TriangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle TriangleB = new Triangle(3, 2.598, 3, 3, 3);
     }
 
 }
