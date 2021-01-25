e0f6db3a28ca6d280d39e5f38d84bc3af5e8e479
Raymond <rchle001@ucr.edu>
2021-01-25 15:42:03 -0800

    Created a class that holds 5 variables and a function that increments the expected grad year variable

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        StudentProfile TonyProfile = new StudentProfile("Tony", "Moraciello", 2034, 3.2, "Computer Engineering");
+
+        System.out.println(TonyProfile.expectGradYear);
+        TonyProfile.IncrementExpectedGradYear();
+        System.out.println(TonyProfile.expectGradYear);
     }
 
 }
--- src/StudentProfile.java
+++ src/StudentProfile.java
@@ -1,6 +1,22 @@
 public class StudentProfile{
-    public static void StudentProfile()
+
+    String firstName;
+    String lastName;
+    int expectGradYear;
+    double GPA;
+    String DeclaredMajor;
+
+    public StudentProfile(String firstName, String lastName, int expectGradYear, double GPA, String DeclaredMajor)
     {
-        
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.expectGradYear = expectGradYear;
+        this.GPA = GPA;
+        this.DeclaredMajor = DeclaredMajor;
     }
+    public void IncrementExpectedGradYear()
+    {
+        this.expectGradYear = this.expectGradYear + 1;
+    }
+
 }
