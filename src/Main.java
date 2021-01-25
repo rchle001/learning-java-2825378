32c7c8589511d79e08bbd9534f1e9c77172814cf
Raymond <rchle001@ucr.edu>
2021-01-25 10:07:15 -0800

    Added function implementation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
+    public static void announcDeveloperTeaTime()
+    {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type in a random word and press Enter to start developer tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time!");
+    }
     public static void main(String[] args) {
 
     }
+
 
 }
